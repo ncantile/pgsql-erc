57d1b64c682018e5e95ecb509f24294e640b3952
ncantile <ncantile@sorint.com>
2022-06-16 15:47:32 +0200

    Updated scripts to be run by a non-superuser:
    d_info_v10 doesn't return largeobjects size and db owner anymore.
    i_info_v10 doesn't return number if pg_hba.conf lines with trust anymore.
    n_info_v10 doesn't return schema owner anymore.

--- erc_grants.sql
+++ erc_grants.sql
@@ -1,6 +1,8 @@
 CREATE USER erc_mon WITH LOGIN PASSWORD 'insert_pwd_here';
-SELECT format('GRANT CONNECT ON DATABASE %I TO erc_mon;', datname) FROM pg_database \gexec --this grants the connection privilege to all databases present in the instance at the moment this script is run
+SELECT format('GRANT CONNECT ON DATABASE %I TO erc_mon;', datname) FROM pg_database \gexec
 SELECT format('GRANT USAGE ON SCHEMA %I TO erc_mon', nspname) FROM pg_namespace \gexec
+ALTER DEFAULT PRIVILEGES FOR ROLE erc_mon GRANT USAGE ON SCHEMAS TO erc_mon;
+ALTER DEFAULT PRIVILEGES FOR ROLE erc_mon IN SCHEMA pg_catalog, information_schema GRANT SELECT ON TABLES TO erc_mon;
 GRANT pg_read_all_settings TO erc_mon;
 GRANT pg_read_all_stats TO erc_mon;
 GRANT SELECT ON ALL TABLES IN SCHEMA pg_catalog TO erc_mon;
